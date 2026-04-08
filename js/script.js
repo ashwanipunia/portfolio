ef4d6c6354f005451d3b49816251a1c7033433ba
ashwanipunia <ashwani.ihbt20a@acsir.res.in>
2026-04-08 12:30:14 +0530

    Delete js/script.js

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const switcher = document.getElementById("profileSwitch");
-
-if (switcher) {
-  switcher.addEventListener("change", e => {
-    document.body.setAttribute("data-profile", e.target.value);
-  });
-}
