fa764a6bc26f31a216b92733ac240ca0496a6855
ashwanipunia <ashwani.ihbt20a@acsir.res.in>
2026-04-08 13:49:20 +0530

    animations.js

--- js/animations.js
+++ js/animations.js
@@ -1,4 +1,9 @@
-const observer = new IntersectionObserver(
+const obs=new IntersectionObserver(e=>{
+e.forEach(x=>x.isIntersecting && x.target.classList.add("visible"));
+},{threshold:0.15});
+
+document.querySelectorAll(".fade-in").forEach(el=>obs.observe(el));
+``const observer = new IntersectionObserver(
   entries => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
